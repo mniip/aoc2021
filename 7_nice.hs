7ee9786da52c980913ba546c0e016f43f9f9cab8
mniip <mniip@mniip.com>
2021-12-07 10:19:03 +0300

    Rewrite with bytestring and vector

--- 7_nice.hs
+++ 7_nice.hs
@@ -1,32 +1,54 @@
 module Main where
 
+import Control.Applicative
 import Control.Monad
-import Data.List (sort)
-import Data.List.Split
+import Control.Monad.ST
 import System.Environment
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+import Data.Attoparsec.ByteString.Char8
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Algorithms.Merge as V
 
 main :: IO ()
 main = do
   args <- getArgs
-  contents <- getContents
+  contents <- BS.getContents
+  let input = readInput contents
   when ("1" `elem` args || null args) $ do
-    printOutput $ solve1 $ readInput1 contents
+    printOutput $ solve1 input
   when ("2" `elem` args || null args) $ do
-    printOutput $ solve2 $ readInput2 contents
+    printOutput $ solve2 input
 
-readInput1 :: String -> [Int]
-readInput1 = map read . splitOn ","
-
-readInput2 :: String -> [Int]
-readInput2 = readInput1
+readInput :: ByteString -> Vector Int
+readInput bs = case parseOnly parser bs of
+  Right r -> r
+  where
+    parser = uncurry V.fromListN <$> go1
+    go1 = do
+      x <- decimal
+      (n, xs) <- go2
+      let n' = n + 1
+      n' `seq` pure (n', x:xs)
+    go2 = (char ',' *> go1) <|> pure (0, [])
 
 printOutput :: Int -> IO ()
 printOutput = print
 
-solve1 :: [Int] -> Int
-solve1 xs = sum [abs (x - p) | x <- xs]
-  where p = sort xs !! (length xs `div` 2)
+solve1 :: Vector Int -> Int
+solve1 v = sum [abs (x - p) | x <- V.toList v]
+  where
+    v' = runST $ do
+      mv <- V.thaw v
+      V.sort mv
+      V.unsafeFreeze mv
+    p = v' V.! (V.length v `div` 2)
 
-solve2 :: [Int] -> Int
-solve2 xs = sum [let n = abs (x - p) in n * (n + 1) `div` 2 | x <- xs]
-  where p = (sum xs + length (filter (\x -> x * length xs > sum xs) xs)) `div` length xs
+solve2 :: Vector Int -> Int
+solve2 v = sum [let n = abs (x - p) in n * (n + 1) `div` 2 | x <- V.toList v]
+  where
+    total = V.sum v
+    len = V.length v
+    p = (total + V.foldl' above 0 v) `div` len
+    above n x = if x * len > total then n + 1 else n

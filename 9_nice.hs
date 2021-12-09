61616a6a06bfac98af7728409e3fd0a4d17cfee8
mniip <mniip@mniip.com>
2021-12-09 08:51:59 +0300

    Make nicer

--- 9_nice.hs
+++ 9_nice.hs
@@ -32,9 +32,5 @@ solve1 a = sum [1 + a ! p | p <- range $ bounds a, all (\p' -> a ! p < a ! p') $
   where neighbors (i, j) = filter (inRange $ bounds a) [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
 
 solve2 :: Array (Int, Int) Int -> Int
-solve2 a = product . take 3 . sortBy (flip compare) . map length . components . fst . graphFromEdges' $ do
-    p <- range bnd
-    guard $ a ! p < 9
-    let p' = first succ p; p'' = second succ p
-    pure ((), p, [p' | inRange bnd p', a ! p' < 9] <> [p'' | inRange bnd p'', a ! p'' < 9])
-  where bnd = bounds a
+solve2 a = product . take 3 . sortBy (flip compare) . map length . components . fst . graphFromEdges'
+  $ [((), p, [first succ p, second succ p]) | p <- range $ bounds a, a ! p < 9]
